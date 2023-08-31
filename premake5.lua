ca4b44ad933fd31bb641faf3b9aefb66c2e8905f
Bruno Vitte <bruno_vitte@hotmail.com>
2023-08-30 23:41:10 -0600

    needs to take de correct screenshots

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+workspace " LibraryManager"
+architecture "x64"
+
+configurations {
+	"Debug",
+	"Release"
+}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "LibraryManager"
+location "LibraryManager"
+kind "ConsoleApp"
+language "C++"
+cppdialect "C++17"
+staticruntime "on"
+
+targetdir("bin/" .. outputdir .. "/%{prj.name}")
+objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+files {
+	"%{prj.name}/src/**.h",
+	"%{prj.name}/src/**.cpp"
+}
+
+filter "configurations:Debug"
+runtime "Debug"
+symbols "On"
+
+filter "configurations:Release"
+runtime "Release"
+symbols "On"
